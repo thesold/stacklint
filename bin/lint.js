60b97240ff88f502a88c699437858d34b8612757
Marco Mark <marco@silvanite.com>
2017-12-04 21:20:46 +0000

    Remove left-over test code

--- bin/lint.js
+++ bin/lint.js
@@ -27,7 +27,7 @@ if (!stacks.length) {
     const stackPathJson = path.resolve(__dirname, '../src/stacks/', stack, 'json')
     const stackPathStubs = path.resolve(__dirname, '../src/stacks/', stack, 'stubs')
 
-    const files = fs.readdir(stackPathStubs, (err, files) => {
+    fs.readdir(stackPathStubs, (err, files) => {
         if (err) {
             console.warn(`No ${stack} stubs found`)
             return
