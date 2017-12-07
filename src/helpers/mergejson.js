d448393bf68cf812986875b402843cafb5be3d5f
Marco Mark <marco@silvanite.com>
2017-12-07 20:00:41 +0000

    Fix issue caused by lint insisting on a return value which causes problems with lodash mergeWith.

--- src/helpers/mergejson.js
+++ src/helpers/mergejson.js
@@ -6,10 +6,9 @@ exports.files = (sourceFile, targetFile) => {
     const sourceJson = fs.existsSync(sourceFile) ? jsonfile.readFileSync(sourceFile) : {}
     const targetJson = fs.existsSync(targetFile) ? jsonfile.readFileSync(targetFile) : {}
 
+    // eslint-disable-next-line consistent-return
     const outputJson = _.mergeWith(targetJson, sourceJson, (objValue, srcValue) => {
         if (_.isArray(objValue)) return _.uniq(objValue.concat(srcValue))
-
-        return objValue
     })
 
     return jsonfile.writeFileSync(targetFile, outputJson, { spaces: 4 })
